522354e15c5390b3ca887a41d27a1de388c48fc6
afranzio <maniafranzio1234@gmail.com>
2022-12-29 11:54:07 +0530

    Exposer to nextjs
    Exposer to nextjs

---
+++ pages/containers/main.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import ContentGenerator from './content'
+import styles from '../../styles/Home.module.css'
+import { Text } from "@nextui-org/react";
+
+export default function Container() {
+    return (
+        <>
+            <div className={styles.center}>
+                <div>
+                    <div className={styles.h1}>
+                        <Text h3 css={{ lineHeight: "$xs", textGradient: "45deg, $yellow600 -20%, $red600 100%", }}>
+                            COVID-19 Tracker
+                        </Text>
+                    </div>
+                </div>
+                <Image
+                    className={styles.logo}
+                    src="/covid.svg"
+                    alt="covid Logo"
+                    width={50}
+                    height={50}
+                    priority
+                />
+            </div>
+            <ContentGenerator />
+        </>
+    )
+}
